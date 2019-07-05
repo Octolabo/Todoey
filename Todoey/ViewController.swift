f25297fd132cd6955f28dbf8c013b3a9c2372113
Ciantury Octojaya <cianturyoctojaya@gmail.com>
2019-07-05 12:24:39 +0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Ciantury Octojaya on 7/5/19.
+//  Copyright © 2019 Ciantury Octojaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
